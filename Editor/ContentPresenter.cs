979876040291de0759465f70a2c3c7a54a639b46
PassivePicasso <james.lapenn@gmail.com>
2021-02-19 16:40:49 -0500

    Allow setting a template to use for a content presenter

--- Editor/ContentPresenter.cs
+++ Editor/ContentPresenter.cs
@@ -22,6 +22,7 @@ namespace VisualTemplates
 
         public new class UxmlTraits : VisualElement.UxmlTraits
         {
+            private UxmlStringAttributeDescription m_template = new UxmlStringAttributeDescription { name = "template" };
             private UxmlStringAttributeDescription m_bindingPath = new UxmlStringAttributeDescription { name = "binding-path" };
             private UxmlStringAttributeDescription m_configMethod = new UxmlStringAttributeDescription { name = "config-method" };
             private UxmlBoolAttributeDescription m_enableDebug = new UxmlBoolAttributeDescription { name = "enable-debug", defaultValue = false };
@@ -34,6 +35,7 @@ namespace VisualTemplates
 
                 contentPresenter.ConfigMethod = m_configMethod.GetValueFromBag(bag, cc);
                 contentPresenter.EnableDebug = m_enableDebug.GetValueFromBag(bag, cc);
+                contentPresenter.Template = m_template.GetValueFromBag(bag, cc);
             }
 
             public override IEnumerable<UxmlChildElementDescription> uxmlChildElementsDescription
@@ -51,6 +53,7 @@ namespace VisualTemplates
 
         public bool EnableDebug { get; set; }
         public string ConfigMethod { get; set; }
+        public string Template { get; set; }
 
         public Func<string, VisualTreeAsset> LoadAsset;
 
@@ -69,70 +72,77 @@ namespace VisualTemplates
             if (boundObject == null) return;
             Clear();
 
-            string dataType = boundObject.targetObject.GetType().Name;
-            boundProperty = GetProperty(boundObject);
-
-            if (boundProperty != null)
-            {
-                switch (boundProperty.propertyType)
-                {
-                    case SerializedPropertyType.ObjectReference:
-                        dataType = boundProperty.objectReferenceValue.GetType().Name;
-                        break;
-                    default:
-                        switch (boundProperty.type)
-                        {
-                            case var type when reg_managedReference.IsMatch(type):
-#if UNITY_2020_1_OR_NEWER
-                    var mrft = boundProperty.managedReferenceFullTypename;
-                    var assemblyName = mrft.Substring(0, mrft.IndexOf(" "));
-                    dataType = mrft.Substring(mrft.IndexOf(" ") + 1);
-
-                    var asm = AppDomain.CurrentDomain.GetAssemblies().SingleOrDefault(assembly => assembly.GetName().Name == assemblyName);
-                    var type = asm.GetType(dataType, false, true);
-                    var parentType = type;
-
-                    //Debug.Log($"type: {type?.Name}");
-                    VisualTreeAsset treeAsset = null;
-                    while (treeAsset == null)
-                    {
-                        treeAsset = LoadAsset(parentType.Name);
-                        if (treeAsset == null)
-                        {
-                            if (parentType.BaseType == null) break;
-                            if (parentType.BaseType == typeof(object)) break;
-
-                            parentType = parentType.BaseType;
-                        }
-                    }
-                    if (treeAsset != null)
-                    {
-                        dataType = dataType.Substring(dataType.LastIndexOf('.') + 1);
-                        visualTreeAsset = LoadAsset(dataType);
-                    }
-#elif UNITY_2018
-#endif
-                                break;
-                            default:
-                                if (dataType == null) break;
-                                dataType = dataType.Substring(dataType.LastIndexOf('.') + 1);
-                                break;
-                        }
-                        break;
-                }
-
-            }
-            visualTreeAsset = LoadAsset(dataType);
+            if (!string.IsNullOrEmpty(Template))
+                visualTreeAsset = LoadAsset(Template);
 
             if (visualTreeAsset == null)
             {
-                //in order to maintain our index we must add a visual element even if we don't find a template for the item.
-                var error = new Label($"Template file not found: BoundType({boundObject.targetObject.GetType().FullName}) Template({dataType}.uxml)");
-                error.AddToClassList("template-error");
-                Add(error);
+                string dataType = boundObject.targetObject.GetType().Name;
+                boundProperty = GetProperty(boundObject);
 
-                return;
+                if (boundProperty != null)
+                {
+                    switch (boundProperty.propertyType)
+                    {
+                        case SerializedPropertyType.ObjectReference:
+                            dataType = boundProperty.objectReferenceValue.GetType().Name;
+                            break;
+                        default:
+                            switch (boundProperty.type)
+                            {
+#if UNITY_2020_1_OR_NEWER
+                                case var type when reg_managedReference.IsMatch(type):
+                                var mrft = boundProperty.managedReferenceFullTypename;
+                                var assemblyName = mrft.Substring(0, mrft.IndexOf(" "));
+                                dataType = mrft.Substring(mrft.IndexOf(" ") + 1);
+
+                                var asm = AppDomain.CurrentDomain.GetAssemblies().SingleOrDefault(assembly => assembly.GetName().Name == assemblyName);
+                                var type = asm.GetType(dataType, false, true);
+                                var parentType = type;
+
+                                //Debug.Log($"type: {type?.Name}");
+                                VisualTreeAsset treeAsset = null;
+                                while (treeAsset == null)
+                                {
+                                    treeAsset = LoadAsset(parentType.Name);
+                                    if (treeAsset == null)
+                                    {
+                                        if (parentType.BaseType == null) break;
+                                        if (parentType.BaseType == typeof(object)) break;
+
+                                        parentType = parentType.BaseType;
+                                    }
+                                }
+                                if (treeAsset != null)
+                                {
+                                    dataType = dataType.Substring(dataType.LastIndexOf('.') + 1);
+                                    visualTreeAsset = LoadAsset(dataType);
+                                }
+                            break;
+#elif UNITY_2018
+#endif
+                                default:
+                                    if (dataType == null) break;
+                                    dataType = dataType.Substring(dataType.LastIndexOf('.') + 1);
+                                    break;
+                            }
+                            break;
+                    }
+
+                }
+                visualTreeAsset = LoadAsset(dataType);
+
+                if (visualTreeAsset == null)
+                {
+                    //in order to maintain our index we must add a visual element even if we don't find a template for the item.
+                    var error = new Label($"Template file not found: BoundType({boundObject.targetObject.GetType().FullName}) Template({dataType}.uxml)");
+                    error.AddToClassList("template-error");
+                    Add(error);
+
+                    return;
+                }
             }
+
 
 #if UNITY_2019_1_OR_NEWER
             visualTreeAsset.CloneTree(this);
